03c8bf48048a91bd4b80c026f9c8bb4165939498
olga-nest <olga.w.nesterova@gmail.com>
2017-10-19 11:21:18 -0700

    Setup default layout, deque cells

--- WavyCollectionView/ViewController.m
+++ WavyCollectionView/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "ONECollectionViewCell.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic) UICollectionViewFlowLayout *defaultLayout;
 
 @end
 
@@ -16,14 +19,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.collectionView.dataSource = self;
+    [self setupDefaultLayout];
+    
+    
+    
+}
+
+-(void)setupDefaultLayout {
+    self.defaultLayout = [[UICollectionViewFlowLayout alloc] init];
+    
+    self.defaultLayout.itemSize = CGSizeMake(100, 100); // Set size of cell
+    self.defaultLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);  // "Border around each section"
+    self.defaultLayout.minimumInteritemSpacing = 15;  // Minimum horizontal spacing between cells
+    self.defaultLayout.minimumLineSpacing = 10;  // Minimum vertical spacing
+        
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 10;
 }
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (__kindof UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    ONECollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"purpleCell" forIndexPath:indexPath];
+    
+    NSString *labelText = [NSString stringWithFormat:@"%ld-%ld", (long)indexPath.section, (long)indexPath.row];
+    
+    cell.purpleViewLabel.text = labelText;
+    
+    return cell;
+}
+
 
 
 @end
