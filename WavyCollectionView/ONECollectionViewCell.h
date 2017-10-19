2a71f74100c06e209f3b456f2426de836c6d52dc
olga-nest <olga.w.nesterova@gmail.com>
2017-10-19 11:20:37 -0700

    Create customCollectionViewCell class

---
+++ WavyCollectionView/ONECollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ONECollectionViewCell.h
+//  WavyCollectionView
+//
+//  Created by Olga on 10/19/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONECollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *purpleViewLabel;
+
+@end
---
+++ WavyCollectionView/ONECollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ONECollectionViewCell.m
+//  WavyCollectionView
+//
+//  Created by Olga on 10/19/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "ONECollectionViewCell.h"
+
+@implementation ONECollectionViewCell
+
+@end
