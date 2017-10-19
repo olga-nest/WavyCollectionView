9dbeccb798e5dc102b67fdb3acd9882c134b769f
olga-nest <olga.w.nesterova@gmail.com>
2017-10-19 10:52:10 -0700

    Initial Commit

---
+++ WavyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavyCollectionView
+//
+//  Created by Olga on 10/19/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
